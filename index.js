8c69bca86c86ce30a7249a86fe69bcda0d7b86e2
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2025-01-16 14:56:30 +0100

    Bump dependencies; Use Node 22 & PNPM; Enable strict mode

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const SVGSpriter    = require('svg-sprite'),
-    path            = require('path'),
-    mkdirp          = require('mkdirp'),
-    fs              = require('fs');
+import SVGSpriter from 'svg-sprite';
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
 
-const ROOT_SVG_DIR = __dirname + '/node_modules/lucide-static/icons/';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const ROOT_SVG_DIR = path.join(__dirname, 'node_modules/lucide-static/icons/');
 
 const config = {
     "svg": {
@@ -22,39 +25,41 @@ const config = {
     }
 };
 
-
 const spriter = new SVGSpriter(config);
-const files   = fs.readdirSync(ROOT_SVG_DIR);
+const files = fs.readdirSync(ROOT_SVG_DIR);
 
 // Register SVG files with the spriter
-files.forEach(function(file){
+files.forEach((file) => {
     spriter.add(
-        path.resolve(ROOT_SVG_DIR + file),
+        path.resolve(ROOT_SVG_DIR, file),
         file,
-        fs.readFileSync(path.resolve(ROOT_SVG_DIR + file), { encoding: 'utf-8' })
+        fs.readFileSync(path.resolve(ROOT_SVG_DIR, file), { encoding: 'utf-8' })
     );
-})
+});
 
 // Compile the sprites
-spriter.compile(function(error, result) {
+spriter.compile((error, result) => {
+    if (error) {
+        console.error('Error compiling sprites:', error);
+        process.exit(1);
+    }
+    
     // Run through all configured output modes
-    for (var mode in result) {
+    for (const mode in result) {
         // Run through all created resources and write them to disk
-        for (var type in result[mode]) {
-            mkdirp.sync(path.dirname(result[mode][type].path));
+        for (const type in result[mode]) {
+            fs.mkdirSync(path.dirname(result[mode][type].path), { recursive: true });
             fs.writeFileSync(result[mode][type].path, result[mode][type].contents);
             cleanupSprite(result[mode][type].path);
         }
     }
 });
 
-
-
 function cleanupSprite (spriteFile)
 {
-    var fileContent = fs.readFileSync(spriteFile, 'utf8');
+    const fileContent = fs.readFileSync(spriteFile, 'utf8');
 
-    var result = fileContent.replaceAll(' xmlns="http://www.w3.org/2000/svg"', '')
+    let result = fileContent.replaceAll(' xmlns="http://www.w3.org/2000/svg"', '')
                             .replaceAll(' xmlns:xlink="http://www.w3.org/1999/xlink"', '')
                             .replace('<svg>', '<svg xmlns="http://www.w3.org/2000/svg">');
 
